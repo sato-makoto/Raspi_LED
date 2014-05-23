f70c4b45e1d29ba5cf0f46f484ed1c4a24b740bb
Sato Makoto <makoto@kuzuore.com>
2014-05-24 08:18:41 +0900

    add escape sequence to four_button/test.py

--- four_button/test.py
+++ four_button/test.py
@@ -14,6 +14,10 @@ def input_check(port):
   button = GPIO.input(port)
   return (button, port)
 
+esc = [31, 37, 33, 30]
+seq_begin = '\033[1;'
+seq_end = '\033[00m'
+
 ports = [28, 29, 30, 31]
 for port in ports:
   GPIO.setup(port, GPIO.IN)
@@ -23,12 +27,16 @@ while True:
     check = input_check(port)
     if check[0] == False:
       if port == ports[0]:
-        print '赤い釦が押されました'
+        print '{0}{1}m赤い釦{2}が押されました' . \
+          format(seq_begin, esc[0], seq_end)
       elif port == ports[1]:
-        print '白い釦が押されました'
+        print '{0}{1}m白い釦{2}が押されました' . \
+          format(seq_begin, esc[1], seq_end)
       elif port == ports[2]:
-        print '黄の釦が押されました'
+        print '{0}{1}m黄の釦{2}が押されました' . \
+	  format(seq_begin, esc[2], seq_end)
       elif port == ports[3]:
-        print '灰の釦が押されました'
+        print '{0}{1}m灰の釦{2}が押されました' . \
+	  format(seq_begin, esc[3], seq_end)
       while check[0] == False:
         check = input_check(port)
