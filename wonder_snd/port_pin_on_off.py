3eaf4476d1a6159430f3a245fa4ae13c372b49d6
Sato Makoto <makoto@kuzuore.com>
2015-05-04 13:57:12 +0900

    new: changed port_pin_on_off to ensure input data, from list to tupple

--- wonder_snd/port_pin_on_off.py
+++ wonder_snd/port_pin_on_off.py
@@ -3,6 +3,11 @@
 
 import RPi.GPIO as GPIO
 
+# sound on, off, etc.
+# need to import on, off, aoff, clean
+# argumet is one of 1,2,3,4,5,6
+# wrong argument become 1
+
 """
 PORT  JP
 24    1
@@ -13,22 +18,29 @@ PORT  JP
 23    6
 
 """
-
-plist = [24, 17, 18, 27, 22, 23]
+ptupple = (24, 17, 18, 27, 22, 23)
 hlist = set([])
 
 GPIO.setmode(GPIO.BCM)
 
+def check(pin):
+  if 0 < pin < 7:
+    return int(pin)
+  else:
+    return 1
+
 def on(pin):
+  pin = check(pin)
   pin = pin - 1
-  GPIO.setup(plist[pin], GPIO.OUT)
-  GPIO.output(plist[pin], GPIO.HIGH)
-  hlist.add(plist[pin])
+  GPIO.setup(ptupple[pin], GPIO.OUT)
+  GPIO.output(ptupple[pin], GPIO.HIGH)
+  hlist.add(ptupple[pin])
 
 def off(pin):
+  pin = check(pin)
   pin = pin - 1
-  GPIO.output(plist[pin], GPIO.LOW)
-  hlist.remove(plist[pin])
+  GPIO.output(ptupple[pin], GPIO.LOW)
+  hlist.remove(ptupple[pin])
 
 def aoff():
   for num in hlist:
