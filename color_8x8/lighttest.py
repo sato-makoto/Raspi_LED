e109f660ba996cca93bf795bb48f8709c1a5e134
Sato Makoto <makoto@kuzuore.com>
2015-08-11 23:28:46 +0900

    first color_8x8

---
+++ color_8x8/lighttest.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from sys import argv 
+import time
+import RPi.GPIO as GPIO
+import random
+
+try:
+  repeat_time = int(argv[2])
+except:
+  repeat_time = 10
+
+sleeptime = 3
+
+GPIO.setmode(GPIO.BCM)
+
+p_port = 12
+g_port = 17
+
+GPIO.setup(g_port, GPIO.OUT)
+GPIO.setup(p_port, GPIO.OUT)
+
+GPIO.output(p_port,GPIO.HIGH)
+time.sleep(sleeptime)
+GPIO.output(p_port,GPIO.LOW)
+GPIO.setup(p_port, GPIO.IN)
+GPIO.setup(g_port, GPIO.IN)
+
+GPIO.cleanup()
